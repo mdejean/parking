4edfb9be40cb2eb211736545748a000c4c29068b
Marcel Dejean <marcel@dejean.nyc>
2020-12-26 12:01:21 -0500

    directory cleanup

--- import.sh
+++ import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-gcc -Wall -Werror blockface.c -L/opt/geosupport/lib/ -lgeo -lapequiv -ledequiv -lsan -lsnd -lstExcpt -lStdLast -lStdUniv -lstEnder -lstretch -lthined -lm -lc -lgcc_s -ldl -o blockface
+gcc -Wall -Werror geosupport/blockface.c -L/opt/geosupport/lib/ -lgeo -lapequiv -ledequiv -lsan -lsnd -lstExcpt -lStdLast -lStdUniv -lstEnder -lstretch -lthined -lm -lc -lgcc_s -ldl -o blockface
 
 psql postgres -c "drop database if exists parking;"
 createdb parking 
@@ -16,7 +16,7 @@ shp2pgsql -I -D -s 2263 import/lion.shp street_segment | psql -v ON_ERROR_STOP=1
 shp2pgsql -I -D -s 2263 import/nybb.shp borough | psql -v ON_ERROR_STOP=1 parking
 shp2pgsql -I -D -s 2263 import/nyct2010wi.shp census_tract | psql -v ON_ERROR_STOP=1 parking
 shp2pgsql -I -D -s 2263 import/nycb2010wi.shp census_block | psql -v ON_ERROR_STOP=1 parking
-shp2pgsql -I -D -s 4326 -m columns.txt import/Parking_Regulation_Shapefile/Parking_Regulation_Shapefile.shp parking_regulation | psql -v ON_ERROR_STOP=1 parking
+shp2pgsql -I -D -s 4326 -m import/columns.txt import/Parking_Regulation_Shapefile/Parking_Regulation_Shapefile.shp parking_regulation | psql -v ON_ERROR_STOP=1 parking
 shp2pgsql -I -D -s 2263 import/NYC_Hydrants/NYCDEP_Hydrants.shp hydrant | psql -v ON_ERROR_STOP=1 parking
 
 psql -v ON_ERROR_STOP=1 -f blockface_geom.sql parking
