74967bb75aeb44c9c38733229f0b62e91d3673cc
Marcel Dejean <marcel@dejean.nyc>
2021-01-29 00:51:27 -0500

    increase max zoom

--- map/map.js
+++ map/map.js
@@ -7,7 +7,8 @@ var map = L.map('map');
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    className: 'tiles'
+    className: 'tiles',
+    maxZoom: 19
 }).addTo(map);
 
 
